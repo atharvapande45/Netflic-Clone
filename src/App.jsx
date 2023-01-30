8e359c9a385b71be945a7e5c175b828e36d2b225
atharvapande45 <atharva.pande20@pccoepune.org>
2023-01-31 00:04:18 +0530

    added row components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import "./App.css";
 import Row from "./Components/Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div>
       <h1>MetFlix Clone</h1>
-      <Row title="MetFlix Originals" />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending now" fetchURL={requests.fetchTrending} />
+
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
